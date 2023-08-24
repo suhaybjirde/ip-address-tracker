c93d05b303081f3af153c9fdf0f445a407d5d236
Suhayb Jirde <dribblekayga@gmail.com>
2023-08-23 23:03:58 -0800

    fix the marker not showing

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
-
+import { Icon } from "leaflet";
+import iconImg from '../assets/marker-icon.png'
 
 function SetTheView({latitude, longitude}) {
     const map = useMap()
@@ -10,15 +11,20 @@ function SetTheView({latitude, longitude}) {
 
 const Map = ({latitude, longitude})=> {
 
+    const icon = new Icon({
+        iconUrl: iconImg,
+        iconSize: [38, 38]
+    })
+
     return (
         <MapContainer center={[latitude, longitude]} zoom={13} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[latitude, longitude]}>
+            <Marker position={[latitude, longitude]} icon={icon}>
                 <Popup>
-                IP here
+                Here is the IP Address
                 </Popup>
             </Marker>
             <SetTheView  latitude={latitude} longitude={longitude} />
